c71c3c37facd82c052a495d121ab151f578c3c5a
Nik Trush <nick_trush@mail.ru>
2021-04-16 22:25:54 +0300

    Это База

---
+++ BRD_Sem/BRD_Sem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BRD_Sem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
